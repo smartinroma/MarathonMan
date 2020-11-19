fe138a6ae355c87f3df784455fc0d23326cf150c
gonzalo-marin <gonzalo.marn@gmail.com>
2020-11-19 17:45:17 +0100

    interfaz, servicios getAllEvents y addEvent

--- src/app/services/principal.service.ts
+++ src/app/services/principal.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
 
+export interface Event {
+  titulo: string;
+  nivel: string;
+  fecha: Date;
+  hora: number;
+  imagen: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class PrincipalService {
 
+  private events: Event[];
+
   constructor() { }
+
+  getAllEvents(): Promise<Event[]> {
+    return new Promise((resolve, reject) => {
+      resolve(this.events);
+    });
+  }
+
+  addEvent(pEvent: Event): Promise<string> {
+    return new Promise((resolve, reject) => {
+      this.events.push(pEvent);
+      console.log(this.events);
+    });
+  }
+
 }

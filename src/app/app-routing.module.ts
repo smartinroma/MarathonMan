d35c432719c8e2f96703ab6648b6e524290bb2c8
Sofia Martin Román <s.martinroma93@gmail.com>
2020-12-10 15:41:06 +0100

    pagina intro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { ComentariosComponent } from './comentarios/comentarios.component';
 import { EnlacesComponent } from './enlaces/enlaces.component';
 import { EventoFormularioComponent } from './evento-formulario/evento-formulario.component';
+import { IntroComponent } from './intro/intro.component';
 import { MainComponent } from './main/main.component';
 import { PerfilComponent } from './perfil/perfil.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/main' },
+  { path: '', pathMatch: 'full', redirectTo: '/intro' },
+  { path: 'intro', component: IntroComponent },
   { path: 'main', component: MainComponent },
   { path: 'formulario', component: EventoFormularioComponent },
   { path: 'perfil', component: PerfilComponent },
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'MarathonMan';
+  url: string = '';
+  visible: boolean;
+
+  constructor() {
+    this.visible = false;
+
+  }
+
+
+  ngOnInit() {
+    console.log(window.location.pathname);
+
+    if (window.location.pathname !== "/intro" && window.location.pathname !== "/") {
+      this.visible = true;
+    }
+  }
 }

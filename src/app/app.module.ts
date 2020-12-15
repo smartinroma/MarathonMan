748d6e2f58bb6bed7f395f1f57c524bd651d9f2d
gonzalo-marin <gonzalo.marn@gmail.com>
2020-12-15 12:01:10 +0100

    importamos FormsModule en app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { EventoFormularioComponent } from './evento-formulario/evento-formulario.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PerfilComponent } from './perfil/perfil.component';
 import { EnlacesComponent } from './enlaces/enlaces.component';
 import { ComentariosComponent } from './comentarios/comentarios.component';
@@ -30,7 +30,8 @@ import { LoginComponent } from './login/login.component';
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
